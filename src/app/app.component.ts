3696ade2f261f93eb4fc10e1ae7bc7c0abe90daa
Rox Teddy <roxteddy.paris@gmail.com>
2023-05-04 12:31:32 +0200

    design integration almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { delay, timeout } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { SongUploadComponent } from './song-upload/song-upload.component';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 export interface Song {
     id: number
@@ -25,7 +27,38 @@ export class AppComponent implements OnInit {
     selectedSong?: Song;
     SERVER_URL = SERVER_URL;
     constructor(private readonly dialog: MatDialog,
-    private readonly http: HttpClient) {}
+                private readonly domSanitizer: DomSanitizer,
+                private readonly matIconRegistry: MatIconRegistry,
+                private readonly http: HttpClient) {
+        this.matIconRegistry.addSvgIcon(
+            `m`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/svg/m.svg`)
+        );
+        this.matIconRegistry.addSvgIcon(
+            `s`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/svg/s.svg`)
+        );
+        this.matIconRegistry.addSvgIcon(
+            `drums`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/svg/drums.svg`)
+        );
+        this.matIconRegistry.addSvgIcon(
+            `piano`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/svg/piano.svg`)
+        );
+        this.matIconRegistry.addSvgIcon(
+            `bass`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/svg/bass.svg`)
+        );
+        this.matIconRegistry.addSvgIcon(
+            `vocals`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/svg/vocals.svg`)
+        );
+        this.matIconRegistry.addSvgIcon(
+            `other`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/svg/other.svg`)
+        );
+    }
 
     ngOnInit(): void {
         this.getSongs();
