b38dcdb2149840ad104ca45ebe37f60d1a3fbeaf
roxteddy <roxteddy.paris@gmail.com>
2023-05-31 01:50:42 +0000

    Deploying to gh-pages from  - 3792940a3688f624845fa13dd164f6c01cdd810f 🚀

--- assets/scripts/my-player.js
+++ assets/scripts/my-player.js
@@ -113,7 +113,7 @@ class MyPlayer extends  SuperpoweredWebAudio.AudioWorkletProcessor {
                 type: 'load',
                 name,
                 success: true,
-                analysis: this.analyze(decodedAudio)
+                //analysis: this.analyze(decodedAudio)
             });
         } else {
             this.sendMessageToMainScope({
@@ -287,7 +287,7 @@ class MyPlayer extends  SuperpoweredWebAudio.AudioWorkletProcessor {
                 buffersize
             );
 
-            if (this.stems.length > 3) {
+            if (this.stems.length > 4) {
                 this.mixer.process(
                     this.stems[4]?.buffer?.pointer || 0,
                     this.stems[5]?.buffer?.pointer || 0,
